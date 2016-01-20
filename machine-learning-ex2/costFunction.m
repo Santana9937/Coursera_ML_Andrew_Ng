615fa528eee08d573a42cc8dd5c265a8174c9d2d
Santana9937 <Santana9937@gmail.com>
2016-01-19 16:34:12 -0800

    Finished unregularized logistic regression

--- machine-learning-ex2/costFunction.m
+++ machine-learning-ex2/costFunction.m
@@ -20,13 +20,20 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+% Sigmoid function argument. size(X)=(m, n+1) and size(theta) = (n+1,1)
+% Thus, size(z)=(m,1)
+z = X*theta ; 
+% Hypothesis function is size (m,1)
+htheta = sigmoid(z) ; 
+% Cost function is the summation of two (m,1) vectors over all m
+% observation. Can accomplish this with a dot product.
+J = (1.0/m).*( -transpose(y)*log(htheta) - transpose((1.0-y))*log(1.0-htheta) ) ;
 
-
-
-
-
-
-
+% Error between hypothesis and observations size = (m,1)
+errors = htheta - y ;
+% Size of gradient is (n+1,1). size(X)=(m,n+1). size(errors)=(m,1)
+grad = (1.0/m).*( transpose(X)*errors ) ; 
+ 
 % =============================================================
 
 end
--- machine-learning-ex2/predict.m
+++ machine-learning-ex2/predict.m
@@ -15,7 +15,22 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
+% Sigmoid function argument. size(X)=(m, n+1) and size(theta) = (n+1,1)
+% Thus, size(z)=(m,1)
+z = X*theta ; 
+% Hypothesis function is size (m,1)
+htheta = sigmoid(z) ; 
 
+for i=1:1:m
+    % If hypothesis function >= 0.5 (threshold), predict 1
+    if htheta(i) >= 0.5
+        p(i)=1 ;
+    % Otherwise, predict 0
+    else
+        p(i)=0 ; 
+    end
+end
+    
 
 
 
